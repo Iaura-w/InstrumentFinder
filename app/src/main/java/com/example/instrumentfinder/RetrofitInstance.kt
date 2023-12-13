e9d3db2914f99828c479f8aca239e7544c1e1546
Iaura-w <laura.wo19@gmail.com>
2023-12-13 17:29:34 +0100

    fix server connection

--- app/src/main/java/com/example/instrumentfinder/RetrofitInstance.kt
+++ app/src/main/java/com/example/instrumentfinder/RetrofitInstance.kt
@@ -2,12 +2,19 @@ import com.example.instrumentfinder.FileUploadService
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitInstance {
     private const val BASE_URL = "https://inst-rec.onrender.com/"
 
     private val retrofit: Retrofit by lazy {
-        val client = OkHttpClient.Builder().build()
+        val client = OkHttpClient.Builder()
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(180, TimeUnit.SECONDS)
+            .writeTimeout(180, TimeUnit.SECONDS)
+            .callTimeout(180, TimeUnit.SECONDS)
+            .retryOnConnectionFailure(true)
+            .build()
 
         Retrofit.Builder()
             .baseUrl(BASE_URL)
