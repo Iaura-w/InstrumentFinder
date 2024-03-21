99546400ce0d8c5d6d2e47d1f2a4136f2fee8da2
Iaura-w <laura.wo19@gmail.com>
2024-03-21 15:39:58 +0100

    update

--- app/src/main/java/com/example/instrumentfinder/MainActivity.kt
+++ app/src/main/java/com/example/instrumentfinder/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -25,19 +26,23 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
@@ -66,15 +71,15 @@ class MainActivity : ComponentActivity() {
 
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Preview
     @Composable
     fun FileUpload() {
-        var selectedOption by remember { mutableStateOf(0) }
         val recordAudioIntent = Intent(MediaStore.Audio.Media.RECORD_SOUND_ACTION)
 
         var selectedFileUri by remember { mutableStateOf<Uri?>(null) }
         val viewModel: FileUploadViewModel by viewModels()
-        var serverResponse = ""
+        var serverResponse: String
         val context = LocalContext.current
 
         val filePickerLauncher: ActivityResultLauncher<String> = rememberLauncherForActivityResult(
@@ -83,16 +88,20 @@ class MainActivity : ComponentActivity() {
             selectedFileUri = uri
         }
 
-        ElevatedCard(
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
+                .fillMaxSize()
                 .padding(16.dp)
         ) {
-            Column(
+            TopAppBar(
+                title = { Text(getString(R.string.app_name)) },
+                modifier = Modifier.background(MaterialTheme.colorScheme.primary)
+            )
+
+            ElevatedCard(
                 modifier = Modifier
+                    .fillMaxWidth()
                     .padding(16.dp)
-                    .fillMaxSize(),
-                verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
                 Row(
                     modifier = Modifier
@@ -110,7 +119,7 @@ class MainActivity : ComponentActivity() {
                         Text("Record")
                     }
 
-                    OutlinedButton(onClick = {
+                    FilledTonalButton(onClick = {
                         val intent = Intent(context, HistoryActivity::class.java)
                         intent.putExtra("HISTORY_LIST", ArrayList(viewModel.uploadHistory))
                         startActivity(intent)
@@ -118,99 +127,121 @@ class MainActivity : ComponentActivity() {
                         Text("History")
                     }
                 }
+            }
 
+            selectedFileUri?.let { uri ->
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+                ) {
+                    Column(modifier = Modifier.padding(16.dp)) {
+                        val fileName = uriContentToUriFile(
+                            this@MainActivity,
+                            uri
+                        )?.lastPathSegment ?: "Unknown"
 
-                selectedFileUri?.let { uri ->
-                    val fileName = uriContentToUriFile(
-                        this@MainActivity,
-                        uri
-                    )?.lastPathSegment ?: "Unknown"
-                    Card(
-                        modifier = Modifier.fillMaxWidth(),
-                        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
-                    ) {
-                        Column(modifier = Modifier.padding(16.dp)) {
-                            Text(
-                                "Selected File: $fileName",
-                                style = MaterialTheme.typography.bodySmall
-                            )
-                        }
-                    }
-                    Log.d(TAG, serverResponse)
-
-                    if (viewModel.loading) {
-                        CircularProgressIndicator()
+                        Text("File Upload", style = MaterialTheme.typography.titleSmall)
                         Spacer(modifier = Modifier.height(8.dp))
-                        OutlinedButton(onClick = { viewModel.cancelUpload() }) {
-                            Text("Cancel")
-                        }
-                    } else {
-                        ElevatedButton(onClick = {
-                            val uriFile = uriContentToUriFile(this@MainActivity, uri)
+                        HorizontalDivider()
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Text(
+                            fileName,
+                            style = MaterialTheme.typography.bodySmall
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
 
-                            Log.d(TAG, "uri file: $uriFile")
-
-                            serverResponse = "Uploading file..."
-                            if (uriFile != null) {
-                                viewModel.uploadFile(uriFile, fileName, context)
-                            } else {
-                                serverResponse = "File does not exist"
+                        if (viewModel.loading) {
+                            LinearProgressIndicator(
+                                modifier = Modifier.align(Alignment.CenterHorizontally)
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                            OutlinedButton(
+                                onClick = { viewModel.cancelUpload() },
+                                modifier = Modifier.align(Alignment.CenterHorizontally),
+                            ) {
+                                Text("Cancel")
                             }
+                        } else {
+                            ElevatedButton(
+                                onClick = {
+                                    val uriFile = uriContentToUriFile(this@MainActivity, uri)
+                                    Log.d(TAG, "uri file: $uriFile")
+                                    serverResponse = "Uploading file..."
+                                    if (uriFile != null) {
+                                        viewModel.uploadFile(uriFile, fileName, context)
+                                    } else {
+                                        serverResponse = "File does not exist"
+                                    }
+                                },
+                                modifier = Modifier.align(Alignment.CenterHorizontally)
+                            ) {
+                                Text("Send")
+                            }
+                        }
+                        Spacer(modifier = Modifier.height(16.dp))
 
-                        }) {
-                            Text("Send")
+                        Text("Server response", style = MaterialTheme.typography.titleSmall)
+                        Spacer(modifier = Modifier.height(8.dp))
+                        HorizontalDivider()
+                        Spacer(modifier = Modifier.height(8.dp))
+
+                        serverResponse = viewModel.serverResponse
+                        if (serverResponse.isNotEmpty()) {
+                            Spacer(modifier = Modifier.height(2.dp))
+                            Text(serverResponse)
                         }
                     }
-
-                    serverResponse = viewModel.serverResponse
-                    Text("Result:\n $serverResponse")
-
                 }
-                Spacer(modifier = Modifier.height(16.dp))
             }
+            Spacer(modifier = Modifier.height(16.dp))
         }
     }
 
-    private fun uriContentToUriFile(context: Context, contentUri: Uri): Uri? {
-        val contentResolver: ContentResolver = context.contentResolver
-        val fileName = getFileNameFromContentUri(context, contentUri)
-
-        try {
-            val inputStream: InputStream? = contentResolver.openInputStream(contentUri)
-            if (inputStream != null) {
-                val outputFile = File(context.cacheDir, fileName.toString())
-                val outputStream = FileOutputStream(outputFile)
-                val buffer = ByteArray(1024)
-                var read: Int
-
-                while (inputStream.read(buffer).also { read = it } > 0) {
-                    outputStream.write(buffer, 0, read)
-                }
-                outputStream.flush()
-                outputStream.close()
-                inputStream.close()
-
-                return outputFile.toUri()
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-
-        return null
-    }
-
-    private fun getFileNameFromContentUri(context: Context, contentUri: Uri): String? {
-        val contentResolver: ContentResolver = context.contentResolver
-        val cursor = contentResolver.query(contentUri, null, null, null, null)
-
-        cursor?.use {
-            if (it.moveToFirst()) {
-                val nameColumnIndex = it.getColumnIndex(OpenableColumns.DISPLAY_NAME)
-                if (nameColumnIndex != -1) {
-                    return it.getString(nameColumnIndex)
-                }
-            }
-        }
-        return null
-    }
 }
+
+
+private fun uriContentToUriFile(context: Context, contentUri: Uri): Uri? {
+    val contentResolver: ContentResolver = context.contentResolver
+    val fileName = getFileNameFromContentUri(context, contentUri)
+
+    try {
+        val inputStream: InputStream? = contentResolver.openInputStream(contentUri)
+        if (inputStream != null) {
+            val outputFile = File(context.cacheDir, fileName.toString())
+            val outputStream = FileOutputStream(outputFile)
+            val buffer = ByteArray(1024)
+            var read: Int
+
+            while (inputStream.read(buffer).also { read = it } > 0) {
+                outputStream.write(buffer, 0, read)
+            }
+            outputStream.flush()
+            outputStream.close()
+            inputStream.close()
+
+            return outputFile.toUri()
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+
+    return null
+}
+
+private fun getFileNameFromContentUri(context: Context, contentUri: Uri): String? {
+    val contentResolver: ContentResolver = context.contentResolver
+    val cursor = contentResolver.query(contentUri, null, null, null, null)
+
+    cursor?.use {
+        if (it.moveToFirst()) {
+            val nameColumnIndex = it.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+            if (nameColumnIndex != -1) {
+                return it.getString(nameColumnIndex)
+            }
+        }
+    }
+    return null
+}
+
